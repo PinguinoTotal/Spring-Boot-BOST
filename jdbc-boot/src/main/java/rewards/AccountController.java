ac40d63bcfdb19757e6a53330fcfc4b4fcda3c57
pinguino total <pinguinototal@hotmail.com>
2024-12-03 23:44:30 -0600

    eliminando lo que tenia y poniendo una base de datos mebebida con h2

--- jdbc-boot/src/main/java/rewards/AccountController.java
+++
@@ -1,9 +0,0 @@
-package rewards;
-
-import org.springframework.web.bind.annotation.RestController;
-
-//le indicamos que este sera un controlador que usaremos para que
-//alberge y gestione nuestros endpoints
-@RestController
-public class AccountController {
-}
--- jdbc-boot/src/main/java/rewards/RewardsRepository.java
+++
@@ -1,4 +0,0 @@
-package rewards;
-
-public class RewardsRepository {
-}
